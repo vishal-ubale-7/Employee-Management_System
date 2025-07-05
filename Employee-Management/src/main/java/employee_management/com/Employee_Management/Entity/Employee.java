3f0781906689165091c9c1e9f5afaf0d22953171
Mr.Vishal Ubale <149001221+vishal-ubale-7@users.noreply.github.com>
2025-07-05 15:09:07 +0530

    Update Employee.java

--- Employee-Management/src/main/java/employee_management/com/Employee_Management/Entity/Employee.java
+++ Employee-Management/src/main/java/employee_management/com/Employee_Management/Entity/Employee.java
@@ -5,6 +5,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+//Entity Class Mapped With Database table 
+
 @Entity
 public class Employee
 {
